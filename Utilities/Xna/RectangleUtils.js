ea1bdb0c0234d20986d553d50103e17da0a25c4a
TLEternal <109238149+01Eternal@users.noreply.github.com>
2025-07-26 17:48:57 -0300

    update Utilities/Xna/RectangleUtils.js

---
+++ Utilities/Xna/RectangleUtils.js
@@ -0,0 +1,10 @@
+import { using } from './ModClasses.js';
+
+using('Microsoft.Xna.Framework');
+using('Microsoft.Xna.Framework.Graphics');
+using('Terraria');
+
+
+export class rec {
+	static instance = Rectangle.new('void .ctor(int x, int y, int width, int height');
+}
