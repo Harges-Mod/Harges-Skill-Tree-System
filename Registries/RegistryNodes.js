93f106100dde6efeae6d8dc1cbc08954b24bb539
TLEternal <109238149+01Eternal@users.noreply.github.com>
2025-07-28 16:07:10 -0300

    Update Design And Using Utilities Data of LoadNC

--- Registries/RegistryNodes.js
+++ Registries/RegistryNodes.js
@@ -1,11 +1,15 @@
 import Node from "../Common/Node.js";
-import * as xna from "../Utilities/XnaUtils.js";
+import LoadNC from "../Utilities/LoadNC.js";
 
-let MakeVector = (x, y) => xna.vec2.instance(x, y);
+LoadNC('Utilities')
+
+let MakeVector = (x, y) => vec2.instance(x, y)
 
 Node.Create("Starter", 1, null, "Textures/DefaultIcon.png", MakeVector(0, 0));
-Node.Create("Begginer", 1, "Starter", "Textures/Class/Melee.png", MakeVector(60, -30));
-Node.Create("Summoner", 1, "Starter", "Textures/Class/Summoner.png", MakeVector(60, 30));
+Node.Create("Begginer", 1, "Starter", "Textures/Class/Melee.png", MakeVector(0, -60)); // down
+Node.Create("Summoner", 1, "Starter", "Textures/Class/Summoner.png", MakeVector(0, 60));
 
-Node.Create("Mage", 1, "Starter", "Textures/Class/Mage.png", MakeVector(-60, 30));
-Node.Create("Ranged", 1, "Starter", "Textures/Class/Ranged.png", MakeVector(-60, -30));
+Node.Create("Mage", 1, "Starter", "Textures/Class/Mage.png", MakeVector(-60, 0));
+Node.Create("Ranged", 1, "Starter", "Textures/Class/Ranged.png", MakeVector(60, 0));
+
+// Node.Create('Boats', 2, 'Mage', 'Textures/DefaultIcon.png', MakeVector(-60, -60))
