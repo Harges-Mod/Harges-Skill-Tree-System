df6d88b0092c918fc597af91037390f760a51875
TLEternal <109238149+01Eternal@users.noreply.github.com>
2025-07-29 02:51:32 -0300

    Logic updated to NodeRequiriment 80% Functional missing Extra

--- Registries/RegistryNodes.js
+++ Registries/RegistryNodes.js
@@ -1,15 +1,63 @@
 import Node from "../Common/SkillTree/Node.js";
+import NodeReq from "../Common/SkillTree/NodeRequirement.js";
 import LoadNC from "../Utilities/LoadNC.js";
 
-LoadNC('Utilities')
+LoadNC("Utilities");
 
-let MakeVector = (x, y) => vec2.instance(x, y)
+let MakeVector = (x, y) => UVector2.instance(x, y);
 
-Node.Create("Starter", 1, null, "Textures/DefaultIcon.png", MakeVector(0, 0));
-Node.Create("Begginer", 1, "Starter", "Textures/Class/Melee.png", MakeVector(0, -60)); // down
-Node.Create("Summoner", 1, "Starter", "Textures/Class/Summoner.png", MakeVector(0, 60));
+Node.Create(
+    "Starter",
+    NodeReq.Create(1, [1]),
+    null,
+    "Textures/DefaultIcon.png",
+    MakeVector(0, 0)
+);
 
-Node.Create("Mage", 1, "Starter", "Textures/Class/Mage.png", MakeVector(-60, 0));
-Node.Create("Ranged", 1, "Starter", "Textures/Class/Ranged.png", MakeVector(60, 0));
+Node.Create(
+    "Begginer",
+    NodeReq.Create(1),
+    "Starter",
+    "Textures/Class/Melee.png",
+    MakeVector(0, -60)
+); // down
 
-// Node.Create('Boats', 2, 'Mage', 'Textures/DefaultIcon.png', MakeVector(-60, -60))
+Node.Create(
+    "Summoner",
+    NodeReq.Create(1),
+    "Starter",
+    "Textures/Class/Summoner.png",
+    MakeVector(0, 60)
+);
+
+Node.Create(
+    "Mage",
+    NodeReq.Create(1),
+    "Starter",
+    "Textures/Class/Mage.png",
+    MakeVector(-60, 0)
+);
+
+Node.Create(
+    "Ranged",
+    NodeReq.Create(1),
+    "Starter",
+    "Textures/Class/Ranged.png",
+    MakeVector(60, 0)
+);
+
+Node.Create(
+    "Boats",
+    NodeReq.Create(2),
+    ["Mage", "Begginer"],
+    "Textures/DefaultIcon.png",
+    MakeVector(-60, -60)
+);
+
+Node.Create(
+    "Boats2",
+    NodeReq.Create(2),
+    ["Ranged", "Begginer"],
+    "Textures/DefaultIcon.png",
+    MakeVector(60, -60)
+);
