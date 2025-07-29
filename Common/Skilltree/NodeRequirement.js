9d5c7b97fae062adfa51b468f1c2529f4afa6b89
TLEternal <109238149+01Eternal@users.noreply.github.com>
2025-07-29 03:08:04 -0300

    Fixed jsDoc ?Array<Item> -> ?Array<number> - ID no instance

--- Common/Skilltree/NodeRequirement.js
+++ Common/Skilltree/NodeRequirement.js
@@ -1,23 +1,30 @@
 export default class NodeRequiriment {
-  
-  static Data = []
-  
-  constructor(level, items = null, extra = [{ type: 'none', value: 'none' }]) {
-    this.level = level
-    this.items = items
-    this.extra = extra
-  }
+    static Data = [];
 
-  /**
-   * @summary Node skill requirement.
-   * @param {number} level - Levels needed to unlock this node.
-   * @param {?Array<Item>} [items=null] - Items needed to unlock this node.
-   * @param {Array<{type: string, value: any}>} [extra=[{type:'none',value:'none'}]] - Extras for requirement (e.g. [{type: 'Hardmode', value: true}])
-   * @returns {NodeRequiriment} Returns the created NodeRequiriment instance.
-   */
-  static Create(level, items = null, extra = [{ type: 'none', value: 'none' }]) {
-    const requirementData = new NodeRequiriment(level, items, extra)
-    NodeRequiriment.Data.push(requirementData)
-    return requirementData
-  }
+    constructor(
+        level,
+        items = null,
+        extra = [{ type: "none", value: "none" }]
+    ) {
+        this.level = level;
+        this.items = items;
+        this.extra = extra;
+    }
+
+    /**
+     * @summary Node skill requirement.
+     * @param {number} level - Levels needed to unlock this node.
+     * @param {?Array<number>} [items=null] - Items id needed to unlock this node.
+     * @param {Array<{type: string, value: any}>} [extra=[{type:'none',value:'none'}]] - Extras for requirement (e.g. [{type: 'Hardmode', value: true}])
+     * @returns {NodeRequiriment} Returns the created NodeRequiriment instance.
+     */
+    static Create(
+        level,
+        items = null,
+        extra = [{ type: "none", value: "none" }]
+    ) {
+        const requirementData = new NodeRequiriment(level, items, extra);
+        NodeRequiriment.Data.push(requirementData);
+        return requirementData;
+    }
 }
